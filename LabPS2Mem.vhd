d647e267feb8a6d10c09098d4458c283eb037bee
Alex Heinz <xale@acm.jhu.edu>
2010-11-11 04:31:05 -0500

    Wrote a test implementation of the top-level entity.

--- LabPS2Mem.vhd
+++ LabPS2Mem.vhd
@@ -17,6 +17,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+use ieee.numeric_std.all;
+
 use WORK.AHeinzDeclares.all;
 
 entity LabPS2Mem is
@@ -46,7 +48,48 @@ end LabPS2Mem;
 
 architecture Structural of LabPS2Mem is
 	
+	-- Internal signals
+	-- Inverted reset switch
+	signal swrst_inv	: std_logic;
+	
+	-- PS/2 reader output
+	signal keycode		: std_logic_vector(7 downto 0);
+	alias KEYCODE_UPPER	: std_logic_vector(3 downto 0) is
+		keycode(7 downto 4);
+	alias KEYCODE_LOWER	: std_logic_vector(3 downto 0) is
+		keycode(3 downto 0);
+	
 begin
 	
+	-- Tie flash controller off (for now)
+	fceb <= AL_OFF;
+	
+	-- Invert reset switch
+	swrst_inv <= NOT swrst;
+	
+	-- Instantiate keyboard reader
+	Keyboard : PS2MakeCodeReader
+	port map
+	(
+		-- Connect PS/2 clock and data
+		ps2_clk_AL => ps2_clk,
+		ps2_data => ps2_dat,
+		
+		-- Connect main clock
+		clk => clk50,
+		
+		-- Connect inverted reset switch
+		reset => swrst_inv,
+		
+		-- Leave the 'newcode' line disconnected
+		newcode => open,
+		
+		-- Connect the 'keycode' bus to a local line
+		keycode => keycode
+	);
+	
+	-- Connect keycode bus to seven-segment display, via hex lookup table
+	ls <= NibbleToHexDigit(unsigned(KEYCODE_UPPER));
+	rs <= NibbleToHexDigit(unsigned(KEYCODE_LOWER));
+	
 end Structural;
-
