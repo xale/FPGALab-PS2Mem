0e598353376a353da39a8c98b0770585f8d910d6
Alex Heinz <xale@acm.jhu.edu>
2010-11-12 16:37:59 -0500

    Wrote implementation for RAM write-controller state-machine entity.

--- RAMWriteController.vhd
+++ RAMWriteController.vhd
@@ -43,10 +43,75 @@ entity RAMWriteController is
 end RAMWriteController;
 
 architecture Behavioral of RAMWriteController is
-
+	
+	-- State machine
+	type RAM_WRITE_SM is
+	(
+		WAIT_START,	-- Waiting for startWrite to go high
+		WRITING,	-- Write request made; waiting for completion
+		WAIT_END	-- Write complete, waiting for startWrite to go low
+	);
+	signal writeState	: RAM_WRITE_SM;
+	attribute init	: string;
+	attribute init of writeState	: signal is "WAIT_START";
+	
+	-- Internal signals
+	-- Internal copy of write address
+	signal writeAddress_internal	: unsigned(23 downto 0);
+	
+	-- Two-segment next-state signals
+	signal nextState		: RAM_WRITE_SM;
+	signal nextWriteRequest	: std_logic;
+	signal nextWriteAddress	: unsigned(23 downto 0);
+	
 begin
-
-	-- FIXME: WRITEME
+	
+	-- State-machine process
+	process (clk, reset)
+	begin
+		
+		-- On reset, reinitialize state machine
+		if (reset = AH_ON) then
+			
+			writeState <= WAIT_START;
+			writeAddress_internal <= (others => '0');
+			writeRequest <= AH_OFF;
+		
+		-- On a clock edge, perform two-segment "advances"
+		elsif rising_edge(clk) then
+			
+			writeState <= nextState;
+			writeRequest <= nextWriteRequest;
+			writeAddress_internal <= nextWriteAddress;
+			
+		end if;
+		
+	end process;
+	
+	-- Next-state logic
+	-- * Start a write when startWrite goes high
+	-- * End writing when writeDone goes high
+	-- * Return to initial state when startWrite goes low
+	nextState <=	WRITING when
+						((writeState = WAIT_START) AND (startWrite = AH_ON)) else
+					WAIT_END when
+						((writeState = WRITING) AND (writeDone = AH_ON)) else
+					WAIT_START when
+						((writeState = WAIT_END) AND (startWrite = AH_OFF)) else
+					writeState;
+	
+	-- Next-write-request logic
+	-- Request a write when writing starts
+	nextWriteRequest <=	AH_ON when (writeState = WRITING) else AH_OFF;
+	
+	-- Next-write-address logic
+	-- Increment address after each write cycle
+	nextWriteAddress <=	(writeAddress_internal + 1) when
+							((writeState = WRITING) AND (writeDone = AH_ON)) else
+						writeAddress_internal;
+						
+	-- Connect writeAddress output to internal address via typecast
+	writeAddress <= std_logic_vector(writeAddress_internal);
 	
 end Behavioral;
 
